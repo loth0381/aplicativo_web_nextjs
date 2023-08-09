45ebcc27c89ba6ccf8e8336721218c283893a788
loth0381 <u19217724@gmail.com>
2023-08-09 17:34:34 -0500

    sexto commit

--- components/ProjectActions.tsx
+++ components/ProjectActions.tsx
@@ -1,74 +1,53 @@
-import { ProjectInterface, UserProfile } from '@/common.types'
-import Image from 'next/image'
+"use client"
 
 import Link from 'next/link'
-import Button from "./Button";
-import ProjectCard from './ProjectCard';
+import Image from 'next/image'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { deleteProject, fetchToken } from '@/lib/actions'
 
 type Props = {
-    user: UserProfile;
+    projectId: string
 }
 
-const ProfilePage = ({ user }: Props) => (
-    <section className='flexCenter flex-col max-w-10xl w-full mx-auto paddings'>
-        <section className="flexBetween max-lg:flex-col gap-10 w-full">
-            <div className='flex items-start flex-col w-full'>
-                <Image src={user?.avatarUrl} width={100} height={100} className="rounded-full" alt="user image" />
-                <p className="text-4xl font-bold mt-10">{user?.name}</p>
-                <p className="md:text-5xl text-3xl font-extrabold md:mt-10 mt-5 max-w-lg">I’m Software Engineer at JSM 👋</p>
-                
-                <div className="flex mt-8 gap-5 w-full flex-wrap">
-                    <Button 
-                        title="Follow" 
-                        leftIcon="/plus-round.svg" 
-                        bgColor="bg-light-white-400 !w-max" 
-                        textColor="text-black-100" 
-                    />
-                    <Link href={`mailto:${user?.email}`}>
-                        <Button title="Hire Me" leftIcon="/email.svg" />
-                    </Link>
-                </div>
-            </div>
+const ProjectActions = ({ projectId }: Props) => {
+    const [isDeleting, setIsDeleting] = useState<boolean>(false)
+    const router = useRouter()
 
-            {user?.projects?.edges?.length > 0 ? (
-                <Image
-                    src={user?.projects?.edges[0]?.node?.image}
-                    alt="project image"
-                    width={739}
-                    height={554}
-                    className='rounded-xl object-contain'
-                />
-            ) : (
-                <Image
-                    src="/profile-post.png"
-                    width={739}
-                    height={554}
-                    alt="project image"
-                    className='rounded-xl'
-                />
-            )}
-       </section>
+    
+    const handleDeleteProject = async () => {
+        setIsDeleting(true)
+        
+        const { token } = await fetchToken();
 
-       <section className="flexStart flex-col lg:mt-28 mt-16 w-full">
-           <p className="w-full text-left text-lg font-semibold">Recent Work</p>
-           
-           <div className="profile_projects">
-                {user?.projects?.edges?.map(
-                    ({ node }: { node: ProjectInterface }) => (
-                        <ProjectCard
-                            key={`${node?.id}`}
-                            id={node?.id}
-                            image={node?.image}
-                            title={node?.title}
-                            name={user.name}
-                            avatarUrl={user.avatarUrl}
-                            userId={user.id}
-                        />
-                    )
-                )}
-            </div>
-       </section>
-   </section>
-)
+        try {
+            await deleteProject(projectId, token);
+            
+            router.push("/");
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsDeleting(false)
+        }
+    }
 
-export default ProfilePage
+    return (
+        <>
+            <Link href={`/edit-project/${projectId}`} className="flexCenter edit-action_btn">
+                <Image src="/pencile.svg" width={15} height={15} alt="edit" />
+            </Link>
+
+            <button
+                type="button"
+                disabled={isDeleting}
+                className={`flexCenter delete-action_btn ${isDeleting ? "bg-gray" : "bg-primary-purple"}`}
+                onClick={handleDeleteProject}
+            >
+                <Image src="/trash.svg" width={15} height={15} alt="delete" />
+            </button>
+        </>
+    )
+}
+
+export default ProjectActions
